067605dec9fea38dc332bebe73a9725374e14774
Lamont Smith <lsmith@techtonicgroup.com>
2017-10-03 16:13:05 -0600

    installed bootstrap, jquery  nuget packages

--- NinjaDomain.DataModel/Class1.cs
+++ NinjaDomain.DataModel/Class1.cs
@@ -6,7 +6,7 @@ namespace NinjaDomain.DataModel
     public class NinjaContext:DbContext
     {
         public DbSet<Ninja> Ninjas { get; set; }
-        public DbSet<Clan>Clans { get; set; }
+        public DbSet<Clan> Clans { get; set; }
         public DbSet<NinjaEquipment> Equipment { get; set; }
     }
 }
